f4c042361caeb4ef36000ad4645d6c8410bcee0b
Claudio Carnino <claudio@tugulab.org>
2024-01-18 18:27:58 +0000

    feat: passing stage 5

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,25 +1,43 @@
 use crate::server::request::HttpRequest;
-use crate::server::response::{HttpResponse, StatusCode};
+use crate::server::response::{Body, HttpResponse, StatusCode};
 
 pub fn handle_request(request: &HttpRequest) -> anyhow::Result<HttpResponse> {
     match request.path.as_str() {
-        "/" => handle_root(request),
-        s if s == "/echo" || s.starts_with("/echo/") => handle_echo(request),
-        _ => handle_unknown(request),
+        "/" => handle_route_root(request),
+        s if s == "/echo" || s.starts_with("/echo/") => handle_route_echo(request),
+        s if s == "/user-agent" || s.starts_with("/user-agent/") => {
+            handle_route_user_agent(request)
+        }
+        _ => handle_route_unknown(request),
     }
 }
 
-fn handle_root(_request: &HttpRequest) -> anyhow::Result<HttpResponse> {
+fn handle_route_root(_request: &HttpRequest) -> anyhow::Result<HttpResponse> {
     Ok(HttpResponse::ok())
 }
 
-fn handle_echo(request: &HttpRequest) -> anyhow::Result<HttpResponse> {
+fn handle_route_echo(request: &HttpRequest) -> anyhow::Result<HttpResponse> {
     let path_parts: Vec<&str> = request.path.split("/").collect();
     let body = path_parts[2..].join("/");
-    let response = HttpResponse::new(StatusCode::Ok, Some(body));
+    let response = HttpResponse::new(StatusCode::Ok, Body::Text(body));
     Ok(response)
 }
 
-fn handle_unknown(_request: &HttpRequest) -> anyhow::Result<HttpResponse> {
+fn handle_route_user_agent(request: &HttpRequest) -> anyhow::Result<HttpResponse> {
+    let user_agent_value = request.headers.iter().find_map(|header| {
+        if header.name == "User-Agent" {
+            Some(header.value.clone())
+        } else {
+            None
+        }
+    });
+    let Some(user_agent_value) = user_agent_value else {
+        anyhow::bail!("Failed to find User-Agent header in request")
+    };
+    let response = HttpResponse::new(StatusCode::Ok, Body::Text(user_agent_value));
+    Ok(response)
+}
+
+fn handle_route_unknown(_request: &HttpRequest) -> anyhow::Result<HttpResponse> {
     Ok(HttpResponse::not_found())
 }
--- src/server/response.rs
+++ src/server/response.rs
@@ -14,27 +14,33 @@ impl ToString for StatusCode {
 }
 
 #[derive(Debug, Clone)]
+pub enum Body {
+    Text(String),
+    None,
+}
+
+#[derive(Debug, Clone)]
 pub struct HttpResponse {
     status_code: StatusCode,
-    body: Option<String>,
+    body: Body,
 }
 
 impl HttpResponse {
     pub fn ok() -> Self {
         HttpResponse {
             status_code: StatusCode::Ok,
-            body: None,
+            body: Body::None,
         }
     }
 
     pub fn not_found() -> Self {
         HttpResponse {
             status_code: StatusCode::NotFound,
-            body: None,
+            body: Body::None,
         }
     }
 
-    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+    pub fn new(status_code: StatusCode, body: Body) -> Self {
         HttpResponse { status_code, body }
     }
 }
@@ -42,11 +48,16 @@ impl HttpResponse {
 impl ToString for HttpResponse {
     fn to_string(&self) -> String {
         let mut response_string = format!("HTTP/1.1 {}\r\n", self.status_code.to_string());
-        if let Some(body) = &self.body {
-            response_string += "Content-Type: text/plain\r\n";
-            response_string += format!("Content-Length: {}\r\n\r\n", body.len()).as_str();
-            response_string += format!("{}\r\n", body).as_str();
+
+        match &self.body {
+            Body::Text(body) => {
+                response_string += "Content-Type: text/plain\r\n";
+                response_string += format!("Content-Length: {}\r\n\r\n", body.len()).as_str();
+                response_string += format!("{}\r\n", body).as_str();
+            }
+            Body::None => {}
         }
+
         response_string += "\r\n";
         response_string
     }
